c65ba8897e876e05dccacb5238d550b5e7767514
Chris <chris.woollon@hotmail.com>
2015-03-21 14:36:05 +0000

    serial comms comments

--- MQTT2Serial/serialComms.py
+++ MQTT2Serial/serialComms.py
@@ -1,23 +1,27 @@
+#import modules
 import time, serial
 import mosquitto
 
+#declare serial connection
 ser = serial.Serial("/dev/tty.usbmodem14211", 9600, timeout=2)
 
-
+#function to run when message received
 def messageReceived(broker, obj, msg):
     global client
     print("Message " + msg.topic + " containing: " + msg.payload)
+    #if message received is "ON" then send "1" over serial port, otherwise if it is "OFF" then send a 0 to the arduino instead
     if msg.payload == "ON":
         ser.write("1")
     elif msg.payload == "OFF":
         ser.write("0")
 
-
-
+#connect to client and subscribe to listen for messages
 client = mosquitto.Mosquitto("Chris")
 client.connect("141.163.83.78")
 client.subscribe("lights")
 client.on_message = messageReceived
+
+#listen for messages
 while True: 
     client.loop()
 
