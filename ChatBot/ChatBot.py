56d50acf0f16345196149be50b56e2ce83587656
Chris <chris.woollon@hotmail.com>
2015-03-21 14:02:42 +0000

    chatbot comments

--- ChatBot/ChatBot.py
+++ ChatBot/ChatBot.py
@@ -3,6 +3,7 @@ import urllib2
 import wikipedia
 from BeautifulSoup import BeautifulSoup
 
+#Opens and stores lists of words
 file = open("StopWords.txt")
 stopwords = file.readlines()
 
@@ -16,6 +17,7 @@ file = open("negative-words.txt")
 negativeWords = file.readlines()
 
 
+#Function for filtering stopwords from input string
 def filterStopWords(output):
     for word in stopwords:
         if input.find(word.strip()) != -1:
@@ -25,7 +27,7 @@ def filterStopWords(output):
             output = output.replace(" " + word.strip() + " ", " <bleep> ")
     return output
 
-
+#function for filtering swearwords from input string
 def identifyPositiveWords(output):
     for word in stopwords:
         if input.find(word.strip()) != -1:
@@ -43,8 +45,10 @@ def identifyPositiveWords(output):
     return(output, emotionGood)
 
 
-
-while True:    
+#begin loop
+while True:
+    
+    #ask user their name and run it through functions to remove words other than their name
     input = raw_input("Hey! What's your name? ")
     filtered = (" " + input.lower() + " ")        
     
@@ -55,7 +59,7 @@ while True:
     filtered = filtered.replace("<bleep>", "")
     filtered = " ".join(filtered.split())
     
-    
+    #if the filtered string ends up blank, or the input was blank to begin with, then catch else carry on and ask the user how they are, running through the same functions to remove words and identify positive/negative sentiment
     if filtered == "":
         print("Tell me your name!")
     else:        
@@ -66,31 +70,34 @@ while True:
     feeling, feelingPositive = identifyPositiveWords(feeling)
     feeling = " ".join(feeling.split())
     
+    
+    #seperate outputs depending on user sentiment. Ask the user what they're thinking about
     if(feelingPositive == True):
         input = raw_input("I'm glad you're feeling " + feeling + " today! What are you thinking about? ")
     elif(feelingPositive == False):
         input = raw_input("I'm sorry to hear you're feeling " + feeling + " today :( What are you thinking about? ") 
         
-        
+    #filter input again    
     topic = (" " + input.lower() + " ")
     
     topic = filterStopWords(topic)
     topic = " ".join(topic.split())
     print(topic)
+    
+    #if input is blank after filtering, then catch and ask again
     while (topic == ""):
         input = raw_input("There isn't really a discussion to be had there.. say something interesting already... ")
         topic = (" " + input.lower() + " ")    
         topic = filterStopWords(topic)
         topic = " ".join(topic.split())
     
+    #use wikipedia module to fetch corresponding wiki page to user's topic
     topic = wikipedia.WikipediaPage(topic)
     print(topic)
     
     print(topic.title)
     print(topic.url)
-#    content = topic.summary
-#    print(content)
-    
+
     print(wikipedia.summary("Paris", sentences=1, chars=100, auto_suggest=True, redirect=True))    
     while True:
         input = raw_input("what an interesting topic " + topic.title + " is. What else are you thinking about? ")
